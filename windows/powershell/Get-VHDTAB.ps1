7a9a93db00ac818ead04e7714b9cbd332aa62f60
Nicolai Brogaard <nmb@minserver.dk>
2016-01-22 15:25:59 +0100

    Revert "Get-vhdtab"
    This reverts commit f31a31e9791da5c41dfc30b283c8c99f1046deaf.

--- windows/powershell/Get-VHDTAB.ps1
+++
@@ -1,7 +0,0 @@
-﻿[CmdletBinding()]
-Param(
-	[Parameter(Mandatory=$True,Position=1)]
-	[string]$ComputerName
-)
-
-GET-VM -ComputerName $ComputerName|Get-VMHardDiskDrive|Select-Object -Property VMName, VMId, ComputerName, ControllerType, ControllerNumber, ControllerLocation, Path|Sort-Object -Property VMName| Out-GridView -Title "Virtual Disks"
